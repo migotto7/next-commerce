9864d235a4e796d5835cc100d4298cd10a9b3346
elthon-migotto <elthon.migotto@specchio.info>
2025-03-27 14:12:58 -0300

    styling component Product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import { ProductType } from "@/types/ProductType";
+import Product from "./components/Product";
+
+
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+
+  if(!res.ok) {
+    throw new Error('failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await getProducts()
+
+
   return (
-    <div>
-      <h1>Next e-commerce</h1>
+    <div className="container mx-auto py-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product: ProductType) =>
+          <Product key={product.id} product={product} />
+        )}
+      </div>
     </div>
   );
 }
