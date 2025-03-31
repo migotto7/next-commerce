eaf4a4af5a7723d245f71a66ec18aded5bee17f0
elthon-migotto <elthon.migotto@specchio.info>
2025-03-31 12:49:32 -0300

    add cart to page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,26 +1,41 @@
-import Link from "next/link"
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Cart from "./Cart";
 
 function Navbar() {
-    return(
-        <nav className='fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300'>
-          <Link href="/" className="uppercase font-bold text-md h-12 flex items-center">
-            Next Store
-          </Link>
-          <div className="flex items-center gap-8">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            <SignedOut>
-              <SignInButton mode="modal">
-                <button className="border rounded-md border-gray-400 px-3 py-2">
-                  Fazer Login
-                </button>       
-              </SignInButton>
-            </SignedOut>
-          </div>
-        </nav>
-    )
+  //const useStore = useCartStore();
+
+  return (
+    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
+      <Link
+        href="/"
+        className="uppercase font-bold text-md h-12 flex items-center"
+      >
+        Next Store
+      </Link>
+      <div className="flex items-center gap-8">
+        <Cart />
+        <div className="flex items-center">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="border rounded-md border-gray-400 px-3 py-2">
+                Fazer Login
+              </button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
